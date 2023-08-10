6fbf5e8b4f83034368008e56cf3fba7354c0dfe4
mickykhd <mickykhd07@gmail.com>
2023-08-10 23:54:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// import { navItems } from "./assets/navMenu.js";
+
+// const navBar = document.querySelector(".nav-menu");
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   const navList = navItems.map(({ id, name }) => {
+//     return ``;
+//   });
+//   navBar.innerHTML = navList.join("");
+// });
---
+++ assets/navMenu.js
@@ -0,0 +1,8 @@
+export const navItems = [
+  { id: 1, name: "Home" },
+  { id: 2, name: "Order" },
+  { id: 3, name: "Food" },
+  { id: 4, name: "Restaurant" },
+  { id: 5, name: "Testimonials" },
+  { id: 6, name: "Contact Us" },
+];
